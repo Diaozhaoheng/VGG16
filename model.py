1b8fe72eb85f4b7afcf450c8555f162fa41da426
dzh <1729451642@qq.com>
2024-09-30 16:41:22 +0800

    VGG16

---
+++ model.py
@@ -0,0 +1,14 @@
+import torch
+from torch import nn
+from torchsummary import summary
+
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+        self.block1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
